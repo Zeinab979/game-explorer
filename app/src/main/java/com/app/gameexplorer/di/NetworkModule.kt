d4ba57b7df340e54b13cdbb4cf656b120999e223
Zeinab979 <zeinabwhdan3677@gmail.com>
2026-03-18 13:06:12 +0200

    provide ApiService and Repository via Hilt modules

---
+++ app/src/main/java/com/app/gameexplorer/di/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.app.gameexplorer.di
+
+import com.app.gameexplorer.data.remote.ApiService
+import com.app.gameexplorer.data.repository.GameRepositoryImpl
+import com.app.gameexplorer.domain.repository.GameRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.rawg.io/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideGameRepository(apiService: ApiService): GameRepository {
+        return GameRepositoryImpl(apiService)
+    }
+}
