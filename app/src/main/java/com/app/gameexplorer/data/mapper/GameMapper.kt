68d8ba58f242c2948a9732928ec7371b847d06b0
Zeinab979 <zeinabwhdan3677@gmail.com>
2026-03-18 13:01:23 +0200

    implement GameMapper to convert API DTOs to Domain models

---
+++ app/src/main/java/com/app/gameexplorer/data/mapper/GameMapper.kt
@@ -0,0 +1,14 @@
+package com.app.gameexplorer.data.mapper
+
+import com.app.gameexplorer.data.remote.dto.GameDto
+import com.app.gameexplorer.domain.model.Game
+
+fun GameDto.toDomain(): Game {
+    return Game(
+        id = id,
+        name = name,
+        backgroundImage = backgroundImage,
+        rating = rating,
+        released = released
+    )
+}
