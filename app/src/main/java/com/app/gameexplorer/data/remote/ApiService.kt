2066c23edbbea5f8c8a627cf24047cdd59847e9f
Zeinab979 <zeinabwhdan3677@gmail.com>
2026-03-18 19:32:53 +0200

    implement ApiService with game details

--- app/src/main/java/com/app/gameexplorer/data/remote/ApiService.kt
+++ app/src/main/java/com/app/gameexplorer/data/remote/ApiService.kt
@@ -1,7 +1,9 @@
 package com.app.gameexplorer.data.remote
 
+import com.app.gameexplorer.data.remote.dto.GameDto
 import com.app.gameexplorer.data.remote.dto.GameResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -13,4 +15,9 @@ interface ApiService {
         @Query("genres") genres: String
     ): GameResponse
 
+    @GET("games/{id}")
+    suspend fun getGameDetail(
+        @Path("id") id: Int,
+        @Query("key") apiKey: String
+    ): GameDto
 }
